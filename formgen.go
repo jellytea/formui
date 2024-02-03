d84a22f1a438d8e4d934643da288071f4ce69177
Jelly Tea <80201595+jellytea@users.noreply.github.com>
2024-02-03 17:24:26 +0000

    Implements BuildFormFromStruct

---
+++ formgen.go
@@ -0,0 +1,34 @@
+// Copyright 2024 JetERA Creative
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0
+// that can be found in the LICENSE file and https://mozilla.org/MPL/2.0/.
+
+package formui
+
+import (
+	"fyne.io/fyne/v2/widget"
+	"reflect"
+)
+
+func BuildFormFromStruct(v any) *widget.Form {
+	var formItems []*widget.FormItem
+
+	t := reflect.TypeOf(reflect.ValueOf(v))
+	for _, f := range reflect.VisibleFields(t) {
+		key := f.Tag.Get("formui")
+		if key == "" {
+			key = f.Name
+		}
+		v, _ := t.FieldByName(f.Name)
+		entry := widget.NewEntry()
+		entry.OnChanged = func(s string) {
+			switch f.Type.Kind() {
+			case reflect.String:
+				reflect.ValueOf(v).SetString(entry.Text)
+			default:
+			}
+		}
+		formItems = append(formItems, widget.NewFormItem(key, entry))
+	}
+
+	return widget.NewForm(formItems...)
+}
